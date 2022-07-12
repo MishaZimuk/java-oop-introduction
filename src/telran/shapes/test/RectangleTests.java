a86a58ce80db0f83eef64195f8a524ee9440058b
MishaZimuk <mishazimuk@gmail.com>
2022-07-12 12:53:27 +0300

    CW09

---
+++ src/telran/shapes/test/RectangleTests.java
@@ -0,0 +1,14 @@
+package telran.shapes.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RectangleTests {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
