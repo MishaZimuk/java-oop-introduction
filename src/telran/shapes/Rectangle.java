f4713687653416b5da456a5b1dd5728ea8fcadc1
MishaZimuk <mishazimuk@gmail.com>
2022-07-12 13:07:18 +0300

    CW09

--- src/telran/shapes/Rectangle.java
+++ src/telran/shapes/Rectangle.java
@@ -3,10 +3,11 @@ package telran.shapes;
 public class Rectangle {
 private int width;
 private int height;
-public Rectangle(int width, int height) {
-	this.width = width;
-	this.height = height;
+public Rectangle( int width, int height) {
+	setWidth(width);
+	setHeight(height);
 }
+/****************getters**************/
 public int getWidth() {
 	return width;
 }
@@ -14,9 +15,23 @@ public int getHeight() {
 	return height;
 }
 public void setWidth(int width) {
+	if (width < 0) {
+		width = -width;
+	}
 	this.width = width;
 }
 public void setHeight(int height) {
+	if (height < 0) {
+		height = -height;
+	}
 	this.height = height;
+}
+public void draw() {
+	for (int i = 0; i < height; i++) {
+		for (int j = 0; j < width; j++) {
+			System.out.print("*" + " ");
+		}
+		System.out.println();
+	}
 }
 }
